ae89da010171f927d38c53e5dcc10f156e3b3c82
Brian Hamlin <brianjhamlin@gmail.com>
2023-07-22 15:12:55 -0500

    changed to hashrouter

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Header />
       <Routes>
         <Route path="/" element={<Homepage />}></Route>
@@ -18,7 +18,7 @@ function App() {
         <Route path="/contact" element={<Contact />}></Route>
       </Routes>
       <Footer />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
