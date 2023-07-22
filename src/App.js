78db4778f0a500418b13af9958049043ccfbceb2
Brian Hamlin <brianjhamlin@gmail.com>
2023-07-22 15:14:37 -0500

    updated hashrouter import

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Homepage from "./components/Homepage";
 import Project from "./components/Project";
 import Contact from "./components/Contact";
 import Resume from "./components/Resume";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
